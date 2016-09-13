0a54698c3375dbd986e75b2aecfe91e636e0739a
Jordan Campbell <jcampbell1441@gmail.com>
2016-09-12 22:46:36 -0500

    fixed port

--- server/index.js
+++ server/index.js
@@ -42,7 +42,7 @@ mongoose.connection.once('open', function() {
 
 
 	// Start server
-	console.log('Listening on port 3000...');
-	app.listen(3000);
+	console.log('Listening on port', process.env.PORT || 3000);
+	app.listen(process.env.PORT || 3000);
 
 })

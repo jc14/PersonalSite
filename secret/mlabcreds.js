72c8e7db0850fc34152afe2585f4447b5be59632
Jordan Campbell <jcampbell1441@gmail.com>
2016-10-09 18:03:16 -0500

    Switched back to mlab

--- secret/mlabcreds.js
+++ secret/mlabcreds.js
@@ -1,1 +1,1 @@
-module.exports = { uri: 'mongodb://jcampbell:KC4yGrVwzvkiK01qDEO8bphZHWmo8ZUPhza5WUfvOBrzjO2pMAtmVObzgqvOhCdGTEW65JMLH2jeFVQDSEkTew==@jcampbell.documents.azure.com:10250/?ssl=true' };
+module.exports = { uri: 'mongodb://admin:password1029@ds023495.mlab.com:23495/jcampbell-tech' };

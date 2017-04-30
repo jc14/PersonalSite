4d523c3565201c455e9ba73e9ed45d4dd6aeb112
Jordan Campbell <jcampbell1441@gmail.com>
2017-04-30 01:42:39 -0500

    Removed bundle.js.map, added styling

--- src/app.js
+++ src/app.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import Main from './main.jsx';
 
+import {
+    indigo500,
+    indigo700,
+    cyan400,
+    black,
+} from 'material-ui/styles/colors';
+
+let muiTheme = getMuiTheme({
+    palette: {
+        primary1Color: indigo500,
+        primary2Color: indigo700,
+        accent1Color: cyan400,
+    }
+});
+
+muiTheme.tabs.backgroundColor = muiTheme.palette.primary2Color;
+
 const App = () => (
-    <MuiThemeProvider>
+    <MuiThemeProvider muiTheme={muiTheme}>
         <Main />
     </MuiThemeProvider>
 );
